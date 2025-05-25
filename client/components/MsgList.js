2064e87dee0e35a63f0cc27ae379291d7f5f51ea
bohodays <bohodays0316@gmail.com>
2025-05-25 11:38:45 +0900

    feat: 메시지 수정, 삭제 기능 구현

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,25 +1,41 @@
+import { useState } from "react";
+import MsgInput from "./MsgInput";
 import MsgItem from "./MsgItem";
 
 const UserIds = ["roy", "jay"];
 
 const getRandomUserId = () => UserIds[Math.round(Math.random())];
 
-const msgs = Array(50)
+const originalMsgs = Array(50)
   .fill(0)
   .map((_, i) => ({
-    id: i + 1,
+    id: 50 - i,
     userId: getRandomUserId(),
-    timestamp: 1234567890123 + i * 1000 * 60,
-    text: `${i + 1} mock text`,
+    timestamp: 1234567890123 + (50 - i) * 1000 * 60,
+    text: `${50 - i} mock text`,
   }));
 
 const MsgList = () => {
+  const [msgs, setMsgs] = useState(originalMsgs);
+  const onCreate = (text) => {
+    const newMsg = {
+      id: msgs.length + 1,
+      userId: getRandomUserId(),
+      timestamp: Date.now(),
+      text: `${msgs.length + 1} ${text}`,
+    };
+    setMsgs((msgs) => [newMsg, ...msgs]);
+  };
+
   return (
-    <ul className="messages">
-      {msgs.reverse().map((item) => (
-        <MsgItem key={item.id} {...item} />
-      ))}
-    </ul>
+    <>
+      <MsgInput mutate={onCreate} />
+      <ul className="messages">
+        {msgs.map((item) => (
+          <MsgItem key={item.id} {...item} />
+        ))}
+      </ul>
+    </>
   );
 };
 
